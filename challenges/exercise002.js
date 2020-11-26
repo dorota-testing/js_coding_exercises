41d0fbec5011f630e6163eea9c41a5a30eb7dfa9
Dorota <d.a.lewinska@gmail.com>
2020-11-26 11:37:15 +0000

    Exercise 2 completed

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,26 +1,46 @@
 function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
   // Your code here!
+  return sandwich.fillings;
 }
 
 function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
   // Your code here!
+  if(person.city === "Manchester"){
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
   // Your code here!
+  
+  if (people % 40 === 0) {
+    return (people / 40);
+  } else {
+    return Math.ceil(people / 40);
+  }
 }
 
 function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
   // Your code here!
+  const result = arr.filter(a => a === "sheep");
+  return result.length;
 }
 
 function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
   // Your code here!
+  let code = person.address.postCode;
+  if(code.charAt(0) === "M" && !isNaN(+(code.charAt(1))) ){
+    return true;
+  } else {
+    return false;
+  }
 }
 
 module.exports = {
