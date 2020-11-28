1b023be4f07673424a4defca92bc92ea4eee44cf
Dorota <d.a.lewinska@gmail.com>
2020-11-28 20:06:58 +0000

    Exercise 7 completed

--- challenges/exercise007.js
+++ challenges/exercise007.js
@@ -4,6 +4,12 @@
  */
 const sumDigits = n => {
   if (n === undefined) throw new Error("n is required");
+  let myArray = n.toString().split('');
+  for (let [key, character] of myArray.entries()){
+    myArray[key] = +(character); //plus changes string into integer
+  }
+  let myNumber = myArray.reduce((a, b) => a + b, 0);
+  return myNumber;
 };
 
 /**
@@ -17,6 +23,18 @@ const sumDigits = n => {
 const createRange = (start, end, step) => {
   if (start === undefined) throw new Error("start is required");
   if (end === undefined) throw new Error("end is required");
+  if (step === undefined) step = 1;
+  let myArray = [start];
+  let n = 0;
+  for (let i = start+1; i < end; i++){    
+    ++n;
+    if( n === step) {
+      n=0; 
+      myArray.push(i);
+    }
+  }
+  myArray.push(end);
+  return myArray;
 };
 
 /**
@@ -51,6 +69,26 @@ const createRange = (start, end, step) => {
 const getScreentimeAlertList = (users, date) => {
   if (users === undefined) throw new Error("users is required");
   if (date === undefined) throw new Error("date is required");
+  let myArray = [];
+
+  for (let key in users) {
+    const screenTime = users[key].screenTime;
+    for (let key2 in screenTime) {
+      if(screenTime[key2].date === date){
+        let usage = screenTime[key2].usage;
+        let arrUsage = Object.values(usage);
+        let myNumber = arrUsage.reduce((a, b) => a + b, 0);
+        //calculate usage
+        //console.log(arrUsage);
+        //console.log("my number =", myNumber);
+        if(myNumber > 100 ){
+          myArray.push(users[key].username);
+        }
+      }
+      //console.log("lorem", users[key].screenTime);
+    }
+  }
+  return myArray;
 };
 
 /**
@@ -65,6 +103,25 @@ const getScreentimeAlertList = (users, date) => {
  */
 const hexToRGB = hexStr => {
   if (hexStr === undefined) throw new Error("hexStr is required");
+  let red = hexStr.substring(1, 3);
+  let green = hexStr.substring(3, 5);
+  let blue = hexStr.substring(5, 7);
+  const convert = str =>{   
+    let first = str.charAt(0);
+    let second = str.charAt(1);
+    const objLetters = {A:10,a:10,B:11,b:11,C:12,c:12,D:13,d:13,E:14,e:14,F:15,f:15}
+    if(first in objLetters){
+      first = objLetters[first];
+    }
+    if(second in objLetters){
+      second = objLetters[second];
+    }    
+    let num = +(first)*16 + +(second);
+    return num;
+  };
+  let strRGB = "rgb(" + convert(red) +","+ convert(green) +","+ convert(blue) +")";
+  //console.log(red, green, blue, lorem);
+  return strRGB;
 };
 
 /**
@@ -79,6 +136,26 @@ const hexToRGB = hexStr => {
  */
 const findWinner = board => {
   if (board === undefined) throw new Error("board is required");
+  let result = null;
+  //console.log(board[1][0]);
+  if (board[0][0] === board[1][0] && board[0][0] === board[0][2]){ //horizontal 1
+    result = board[0][0]; 
+  } else if (board[0][1] === board[1][1] && board[0][1] === board[2][1]) { //horizontal 2
+    result = board[0][1]; 
+  } else if (board[0][2] === board[1][2] && board[0][2] === board[2][2]) { //horizontal 3
+    result = board[0][2];
+  } else if (board[0][0] === board[1][0] && board[0][0] === board[2][0]) { //vertical 1
+    result = board[0][0];
+  } else if (board[1][0] === board[1][1] && board[1][0] === board[1][2]) { //vertical 2
+    result = board[1][0];
+  } else if (board[2][0] === board[2][1] && board[2][0] === board[2][2]) { //vertical 3
+    result = board[2][0];
+  } else if (board[0][0] === board[1][1] && board[0][0] === board[2][2]) { //diagonal 1
+    result = board[0][0];
+  } else if (board[2][0] === board[1][1] && board[1][0] === board[0][2]) { ////diagonal 2
+    result = board[2][0];
+  }
+  return result;
 };
 
 module.exports = {
